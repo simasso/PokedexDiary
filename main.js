baaf7a6dcfcdb230bde32236b453d3c96bfb5429
simasso <simone.massonie@gmail.com>
2025-11-12 16:32:21 +0100

    Feature/simasso/layout cards (#7)
    * layout-cards
    
    * adapt card layout

--- main.js
+++ main.js
@@ -41,21 +41,24 @@ function createCard(data) {
   console.log('POKEMON DATA loaded:', pokeArr);
   const pokeContainer = document.querySelector('#pokemon-container');
   const html = `
-         <article class="bg-teal-900 text-stone-100 flex flex-col items-center rounded-xl shadow">
-          <img src="${data.sprites.front_shiny}" alt="">
-          <h2 class="font-semibold capitalize">${data.name}</h2>
-          <div class="flex gap-2 items-center justify-between w-full px-3">
-            <label for="hp">HP</label>
-            <meter value="${data.stats[0].base_stat}" max="100" id="hp">HP</meter>
-          </div>
-          <div class="flex gap-2 items-center justify-between w-full px-3">
-            <label for="attack">Attack</label>
-            <meter class="[&::-webkit-meter-optimum-value]:bg-red-500" value="${data.stats[1].base_stat}" max="100" id="attack">Attack</meter>
-          </div>
-          <div class="flex gap-2 items-center justify-between w-full px-3 pb-3">
-            <label for="defense">Defense</label>
-            <meter class="[&::-webkit-meter-optimum-value]:bg-blue-500" value="${data.stats[2].base_stat}" max="100"
-              id="defense">Defense</meter>
+         <article class="bg-poke-gray-dark text-stone-100 flex md:flex-col justify-evenly gap-2 rounded-xl shadow">
+          <img class="grow-1" src="${data.sprites.front_shiny}" alt="">
+          
+          <div class="flex flex-col grow-4 justify-center">
+            <h2 class="font-semibold capitalize text-center m-2">${data.name}</h2>
+            <div class="flex gap-2 items-center justify-between w-full px-3">
+              <label for="hp">HP</label>
+              <meter class="bg-poke-yellow" value="${data.stats[0].base_stat}" max="100" id="hp">HP</meter>
+            </div>
+            <div class="flex gap-2 items-center justify-between w-full px-3">
+              <label for="attack">Attack</label>
+              <meter class="bg-poke-red" value="${data.stats[1].base_stat}" max="100" id="attack">Attack</meter>
+            </div>
+            <div class="flex gap-2 items-center justify-between w-full px-3 pb-3">
+              <label for="defense">Defense</label>
+              <meter class="bg-poke-blue" value="${data.stats[2].base_stat}" max="100"
+                id="defense">Defense</meter>
+            </div>
           </div>
         </article>`;
   pokeContainer.insertAdjacentHTML('beforeend', html);
