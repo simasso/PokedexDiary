8d3072bb7788cd4bde2d34029dc6edca530a655d
simasso <simone.massonie@gmail.com>
2025-11-12 18:04:00 +0100

    Feature/simasso/add catch button (#8)
    * layout-cards
    
    * adapt card layout
    
    * add catch button to card

--- main.js
+++ main.js
@@ -41,23 +41,27 @@ function createCard(data) {
   console.log('POKEMON DATA loaded:', pokeArr);
   const pokeContainer = document.querySelector('#pokemon-container');
   const html = `
-         <article class="bg-poke-gray-dark text-stone-100 flex md:flex-col justify-evenly gap-2 rounded-xl shadow">
-          <img class="grow-1" src="${data.sprites.front_shiny}" alt="">
-          
-          <div class="flex flex-col grow-4 justify-center">
-            <h2 class="font-semibold capitalize text-center m-2">${data.name}</h2>
-            <div class="flex gap-2 items-center justify-between w-full px-3">
-              <label for="hp">HP</label>
-              <meter class="bg-poke-yellow" value="${data.stats[0].base_stat}" max="100" id="hp">HP</meter>
-            </div>
-            <div class="flex gap-2 items-center justify-between w-full px-3">
-              <label for="attack">Attack</label>
-              <meter class="bg-poke-red" value="${data.stats[1].base_stat}" max="100" id="attack">Attack</meter>
-            </div>
-            <div class="flex gap-2 items-center justify-between w-full px-3 pb-3">
-              <label for="defense">Defense</label>
-              <meter class="bg-poke-blue" value="${data.stats[2].base_stat}" max="100"
-                id="defense">Defense</meter>
+        <article class="flex flex-col bg-poke-gray-dark text-stone-100 rounded-xl shadow">
+          <button class="w-8 self-end mt-1 me-1 hover:cursor-pointer hover:outline-2 rounded-full flex justify-center">
+            <img src="./assets/icons/pokeball.png">
+          </button>
+          <div class="flex md:flex-col justify-evenly gap-2">
+            <img class="grow-1" src="${data.sprites.front_shiny}" alt="">
+            <div class="flex flex-col grow-4 justify-center">
+              <h2 class="font-semibold capitalize text-center mb-4">${data.name}</h2>
+              <div class="flex gap-2 items-center justify-between w-full px-3">
+                <label for="hp">HP</label>
+                <meter class="bg-poke-yellow" value="${data.stats[0].base_stat}" max="100" id="hp">HP</meter>
+              </div>
+              <div class="flex gap-2 items-center justify-between w-full px-3">
+                <label for="attack">Attack</label>
+                <meter class="bg-poke-red" value="${data.stats[1].base_stat}" max="100" id="attack">Attack</meter>
+              </div>
+              <div class="flex gap-2 items-center justify-between w-full px-3 pb-3">
+                <label for="defense">Defense</label>
+                <meter class="bg-poke-blue" value="${data.stats[2].base_stat}" max="100"
+                  id="defense">Defense</meter>
+              </div>
             </div>
           </div>
         </article>`;
