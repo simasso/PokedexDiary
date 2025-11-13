32d34610d2183bf92a17b669468a70c3cb2111a6
Simone Massonie <simone.massonie@gmail.com>
2025-11-13 10:06:17 +0100

    move createCard into separate script

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+import {
+  createCard,
+  pokeIdFromCatchEvent,
+  catchBtnFromPokeId,
+  pokeIdFromDeleteEvent,
+  deleteBtnFromPokeId,
+} from './utils.js';
 const URL = 'https://pokeapi.co/api/v2/pokemon/';
 const numberToFetch = 10;
-const pfxCatch = 'catch-';
-const pfxDelete = 'delete-';
-const pfxNotes = 'notes-'; // TODO: remove notes button here but keep in pokedex.js
 
 let pokeArr = [];
 
@@ -35,82 +39,26 @@ let pokeArr = [];
 
 /* SCHEDULE ===============================*/
 function createPage(pokeArr) {
-  pokeArr.forEach((element) => {
-    createCard(element);
+  pokeArr.forEach((pokemon) => {
+    createCard(pokemon);
+    catchBtnFromPokeId(pokemon.id).onclick = catchBtnClicked;
+    deleteBtnFromPokeId(pokemon.id).onclick = deleteBtnClicked;
   });
-}
-
-function createCard(data) {
-  console.log('POKEMON DATA loaded:', pokeArr);
-  const isStored = false; // TODO: check if pokemon is stored in favourites
-  const catchBtnVisibility = isStored ? 'hidden' : '';
-  const deleteBtnVisibility = isStored ? '' : 'hidden';
-  const notesBtnVisibility = isStored ? '' : 'hidden'; // TODO: remove notes button here but keep in pokedex.js
-  const pokeContainer = document.querySelector('#pokemon-container');
-  const html = `
-        <article class="flex flex-col bg-poke-gray-dark text-stone-100 rounded-xl shadow">
-          <div class="flex justify-end">
-            <button id="${pfxNotes}${data.id}" class="w-7 mt-1 me-1 hover:cursor-pointer hover:outline-2 rounded-md flex justify-center" ${notesBtnVisibility}>
-              <img class="rounded-md" src="./assets/icons/notes.png">
-            </button>
-            <button id="${pfxDelete}${data.id}" class="w-7 mt-1 me-1 hover:cursor-pointer hover:outline-2 rounded-full flex justify-center" ${deleteBtnVisibility}>
-              <img class="bg-white rounded-full" src="./assets/icons/delete.png">
-            </button>
-            <button id="${pfxCatch}${data.id}" class="w-7 mt-1 me-1 hover:cursor-pointer hover:outline-2 rounded-full flex justify-center" ${catchBtnVisibility}>
-              <img src="./assets/icons/pokeball.png">
-            </button>
-          </div>
-          <div class="flex md:flex-col justify-evenly gap-2">
-            <img class="grow-1" src="${data.sprites.front_shiny}" alt="">
-            <div class="flex flex-col grow-4 justify-center">
-              <h2 class="font-semibold capitalize text-center mb-4">${data.name}</h2>
-              <div class="flex gap-2 items-center justify-between w-full px-3">
-                <label for="hp">HP</label>
-                <meter class="bg-poke-yellow" value="${data.stats[0].base_stat}" max="100" id="hp">HP</meter>
-              </div>
-              <div class="flex gap-2 items-center justify-between w-full px-3">
-                <label for="attack">Attack</label>
-                <meter class="bg-poke-red" value="${data.stats[1].base_stat}" max="100" id="attack">Attack</meter>
-              </div>
-              <div class="flex gap-2 items-center justify-between w-full px-3 pb-3">
-                <label for="defense">Defense</label>
-                <meter class="bg-poke-blue" value="${data.stats[2].base_stat}" max="100"
-                  id="defense">Defense</meter>
-              </div>
-            </div>
-          </div>
-        </article>`;
-  pokeContainer.insertAdjacentHTML('beforeend', html);
-  document.querySelector(`#${pfxNotes}${data.id}`).onclick = notesBtnClicked; // TODO: remove notes button here but keep in pokedex.js
-  document.querySelector(`#${pfxDelete}${data.id}`).onclick = deleteBtnClicked;
-  document.querySelector(`#${pfxCatch}${data.id}`).onclick = catchBtnClicked;
-}
-
-// TODO: remove notes button here but keep in pokedex.js
-function notesBtnClicked(e) {
-  console.log('notes button clicked');
 }
 
 function deleteBtnClicked(e) {
   console.log('delete button clicked');
-  const deleteBtn = e.target.id.startsWith(pfxDelete)
-    ? e.target
-    : e.target.parentElement;
-  const id = deleteBtn.id.split('-').pop();
-  const catchBtn = document.querySelector(`#${pfxCatch}${id}`);
-  deleteBtn.hidden = true;
-  catchBtn.hidden = false;
+  console.log('catch button clicked');
+  const pokeId = pokeIdFromDeleteEvent(e);
+  catchBtnFromPokeId(pokeId).hidden = false;
+  deleteBtnFromPokeId(pokeId).hidden = true;
   // delete Pokemon from favourites
 }
 
 function catchBtnClicked(e) {
   console.log('catch button clicked');
-  const catchBtn = e.target.id.startsWith(pfxCatch)
-    ? e.target
-    : e.target.parentElement;
-  const id = catchBtn.id.split('-').pop();
-  const deleteBtn = document.querySelector(`#${pfxDelete}${id}`);
-  catchBtn.hidden = true;
-  deleteBtn.hidden = false;
+  const pokeId = pokeIdFromCatchEvent(e);
+  catchBtnFromPokeId(pokeId).hidden = true;
+  deleteBtnFromPokeId(pokeId).hidden = false;
   // store pokemon in favourites
 }
---
+++ utils.js
@@ -0,0 +1,89 @@
+const pfxCatch = 'catch-';
+const pfxDelete = 'delete-';
+const pfxNotes = 'notes-';
+
+export function createCard(data, isStored = false) {
+  const catchBtnVisibility = isStored ? 'hidden' : '';
+  const deleteBtnVisibility = isStored ? '' : 'hidden';
+  const notesBtnVisibility = isStored ? '' : 'hidden';
+  const pokeContainer = document.querySelector('#pokemon-container');
+  const html = `
+        <article class="flex flex-col bg-poke-gray-dark text-stone-100 rounded-xl shadow">
+          <div class="flex justify-end">
+            <button id="${pfxNotes}${data.id}" class="w-7 mt-1 me-1 hover:cursor-pointer hover:outline-2 rounded-md flex justify-center" ${notesBtnVisibility}>
+              <img class="rounded-md" src="./assets/icons/notes.png">
+            </button>
+            <button id="${pfxDelete}${data.id}" class="w-7 mt-1 me-1 hover:cursor-pointer hover:outline-2 rounded-full flex justify-center" ${deleteBtnVisibility}>
+              <img class="bg-white rounded-full" src="./assets/icons/delete.png">
+            </button>
+            <button id="${pfxCatch}${data.id}" class="w-7 mt-1 me-1 hover:cursor-pointer hover:outline-2 rounded-full flex justify-center" ${catchBtnVisibility}>
+              <img src="./assets/icons/pokeball.png">
+            </button>
+          </div>
+          <div class="flex md:flex-col justify-evenly gap-2">
+            <img class="grow-1" src="${data.sprites.front_shiny}" alt="">
+            <div class="flex flex-col grow-4 justify-center">
+              <h2 class="font-semibold capitalize text-center mb-4">${data.name}</h2>
+              <div class="flex gap-2 items-center justify-between w-full px-3">
+                <label for="hp">HP</label>
+                <meter class="bg-poke-yellow" value="${data.stats[0].base_stat}" max="100" id="hp">HP</meter>
+              </div>
+              <div class="flex gap-2 items-center justify-between w-full px-3">
+                <label for="attack">Attack</label>
+                <meter class="bg-poke-red" value="${data.stats[1].base_stat}" max="100" id="attack">Attack</meter>
+              </div>
+              <div class="flex gap-2 items-center justify-between w-full px-3 pb-3">
+                <label for="defense">Defense</label>
+                <meter class="bg-poke-blue" value="${data.stats[2].base_stat}" max="100"
+                  id="defense">Defense</meter>
+              </div>
+            </div>
+          </div>
+        </article>`;
+  pokeContainer.insertAdjacentHTML('beforeend', html);
+}
+
+export function catchBtnFromPokemon(pokemon) {
+  const catchBtn = e.target.id.startsWith(pfxCatch)
+    ? e.target
+    : e.target.parentElement;
+  const id = catchBtn.id.split('-').pop();
+  const deleteBtn = document.querySelector(`#${pfxDelete}${id}`);
+  catchBtn.hidden = true;
+  deleteBtn.hidden = false;
+}
+
+export function pokeIdFromCatchEvent(e) {
+  return pokeIdFromEvent(e, pfxCatch);
+}
+
+export function catchBtnFromPokeId(pokeId) {
+  return btnFromPokeId(pokeId, pfxCatch);
+}
+
+export function pokeIdFromDeleteEvent(e) {
+  return pokeIdFromEvent(e, pfxDelete);
+}
+
+export function deleteBtnFromPokeId(pokeId) {
+  return btnFromPokeId(pokeId, pfxDelete);
+}
+
+export function pokeIdFromNotesEvent(e) {
+  return pokeIdFromEvent(e, pfxNotes);
+}
+
+export function notesBtnFromPokeId(pokeId) {
+  return btnFromPokeId(pokeId, pfxNotes);
+}
+
+function btnFromPokeId(pokeId, prefix) {
+  return document.querySelector(`#${prefix}${pokeId}`);
+}
+
+function pokeIdFromEvent(e, prefix) {
+  const btn = e.target.id.startsWith(prefix)
+    ? e.target
+    : e.target.parentElement;
+  return btn.id.split('-').pop();
+}
